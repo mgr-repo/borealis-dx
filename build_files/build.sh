e5437c454c318f717662f584a3322f452eb0e9c3
Martin Grueneberg <lists@martin-grueneberg.de>
2026-04-19 16:55:10 +0200

    feature install basic programms edge, onedrive,  aws cli tools

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,16 +9,50 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# Microsoft Edge key and repo desc
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+curl -fsSL https://packages.microsoft.com/yumrepos/edge/config.repo -o /etc/yum.repos.d/microsoft-edge.repo
 
-#### Example for enabling a System Unit File
+# this installs edge and onedrive
+dnf5 install -y microsoft-edge-stable onedrive
 
-systemctl enable podman.socket
+# edge as default browser 
+xdg-settings set default-web-browser microsoft-edge.desktop || true
+
+TMPDIR="$(mktemp -d)"
+cd "$TMPDIR"
+
+############################
+# AWS CLI v2
+############################
+
+curl -fsSL \
+  "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" \
+  -o awscliv2.zip
+
+dnf install -y unzip
+unzip -q awscliv2.zip
+
+./aws/install \
+  --bin-dir /usr/local/bin \
+  --install-dir /usr/local/aws-cli \
+  --update
+
+############################
+# Session Manager Plugin
+############################
+
+curl -fsSL \
+  "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/linux_x86_64/session-manager-plugin.rpm" \
+  -o session-manager-plugin.rpm
+
+dnf install -y ./session-manager-plugin.rpm
+
+############################
+# Cleanup
+############################
+
+cd /
+rm -rf "$TMPDIR"
+dnf clean all
