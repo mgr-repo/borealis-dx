7345956fcb510f4ada191ada98daac7c1a45d0a8
mgr-repo <54267229+mgr-repo@users.noreply.github.com>
2026-05-21 17:56:00 +0200

    feat: add oc tool install (#17)
    Co-authored-by: Grueneberg, Martin (IT EDP) <martin.grueneberg@boehringer-ingelheim.com>

--- build_files/tasks/02-install-just-scripts.sh
+++ build_files/tasks/02-install-just-scripts.sh
@@ -1,6 +1,7 @@
 ### Install ujust recipes and helper scripts
 install -Dm0644 /ctx/files/ujust/onedrive.just /usr/share/ublue-os/just/onedrive.just
 install -Dm0644 /ctx/files/ujust/aws-smp.just /usr/share/ublue-os/just/aws-smp.just
+install -Dm0644 /ctx/files/ujust/openshift-client.just /usr/share/ublue-os/just/openshift-client.just
 install -Dm0644 /ctx/files/ujust/ssh-key-agent.just /usr/share/ublue-os/just/ssh-key-agent.just
 
 
@@ -10,3 +11,4 @@ install -Dm0644 /ctx/files/ujust/60-custom.just /usr/share/ublue-os/just/60-cust
 install -Dm0755 /ctx/files/scripts/ssh-add-user.sh /usr/share/ublue-os/just/scripts/ssh-add-user.sh
 install -Dm0755 /ctx/files/scripts/onedrive.sh /usr/share/ublue-os/just/scripts/onedrive.sh
 install -Dm0755 /ctx/files/scripts/aws-smp-install.sh /usr/share/ublue-os/just/scripts/aws-smp-install.sh
+install -Dm0755 /ctx/files/scripts/openshift-client-install.sh /usr/share/ublue-os/just/scripts/openshift-client-install.sh
